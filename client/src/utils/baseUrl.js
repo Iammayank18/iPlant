fc536fe035da4968bedbfefea068897a3b5cfdba
Mayank Thakur <mayank.sanyank@gmail.com>
2024-07-06 15:04:21 +0530

    updated credentials

--- client/src/utils/baseUrl.js
+++ client/src/utils/baseUrl.js
@@ -1,10 +1,3 @@
-export const BASE_URL =
-  "https://8050-2405-201-a435-3030-2cc7-c498-e136-e212.ngrok-free.app/";
-// export const BASE_URL = "http://localhost:5001/";
-
-// export const BASE_URL = "";
-// export const GOOGLE_MAPS_APIKEY = "AIzaSyCnH_rrWCOm5v4hvFgUSsz_hUpTvwdjcBI";
-export const GOOGLE_MAPS_APIKEY = "AIzaSyDaQptWy1axlsnLhfrMoMYzuBmdL65tpRE";
-
-export const ORS_APIKEY =
-  "5b3ce3597851110001cf6248800a19756ba344c5b2d53b1857cd1258";
+export const BASE_URL = ''
+export const GOOGLE_MAPS_APIKEY = "";
+export const ORS_APIKEY ="";
